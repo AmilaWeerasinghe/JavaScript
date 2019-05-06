75ee229b9dccc301430f984e55aacf45e8b05f65
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2019-05-06 14:13:37 +0530

    Types and Objects and Notations

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 //first JS
 console.log('Hello World');
+
+let name='Amila';
+const interestRate=1;
+console.log(name);
+console.log(interestRate);
+
+//Objects
+let person ={
+ nameOne:'Amila',
+ ageOne: 23
+
+};
+//dotNotation
+person.ageOne=30;
+
+console.log(person.ageOne);
+
+//Bracket Notation
+person['nameOne']='Pasan';
+
+console.log(person.nameOne);
+
+
